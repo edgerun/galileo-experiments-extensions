2c11fb4d44ce0fd9964c05aa2be4cc87220d6576
Paul Prüller <>
2022-07-26 16:16:24 +0200

    add sleepdetection

---
+++ galileoexperimentsextensions/sleepdetection/profiling/main.py
@@ -0,0 +1,66 @@
+import logging
+import sys
+
+from galileo.shell.shell import init
+from galileo.worker.context import Context
+from galileoexperiments.api.model import ProfilingWorkloadConfiguration
+from galileoexperiments.experiment.profiling.run import run_profiling_workload
+
+from galileoexperimentsextensions.sleepdetection.app import SleepDetectionProfilingApplication
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    logging.basicConfig(level=logging._nameToLevel['INFO'])
+
+    if len(sys.argv) != 6:
+        raise ValueError(
+            'Program takes exactly six arguments: <creator> <host> <container-image> <zone> <master-node>')
+
+    creator = sys.argv[1]
+    host = sys.argv[2]
+    image = sys.argv[3]
+    zone = sys.argv[4]
+    master_node = sys.argv[5]
+
+    # Instantiate galileo context that includes all dependencies needed to execute an experiment
+    ctx = Context()
+    rds = ctx.create_redis()
+    g = init(rds)
+
+    # Configure Sleepdetection specific parameters (i.e., image_url) and define the function name
+    params = {
+        'service': {
+            'name': 'sleepdetection',
+            # Sleeping person in picture
+            'location': 'https://i.imgur.com/bqnrbv6.jpg',
+            # No sleeping person in picture
+            # 'location': 'https://i.imgur.com/XWPElmP.jpg',
+            'remote': True
+        }
+    }
+
+    # Instantiate the Profiling Application that contains methods that spawn a container and return a ClientGroup
+    sleepdetection_profiling_app = SleepDetectionProfilingApplication()
+
+    workload_config = ProfilingWorkloadConfiguration(
+        creator=creator,
+        app_name='sleepdetection',
+        host=host,
+        image=image,
+        master_node=master_node,
+        zone=zone,
+        context=g,
+        params=params,
+        profiling_app=sleepdetection_profiling_app,
+        no_pods=1,
+        n=10,
+        ia=0.5,
+        n_clients=1
+    )
+    run_profiling_workload(workload_config)
+
+
+if __name__ == '__main__':
+    main()

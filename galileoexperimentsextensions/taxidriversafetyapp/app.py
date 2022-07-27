7dece1f20dd8b2d00fab3e99f0c5255526bcfdfb
Paul Prüller <>
2022-07-27 15:39:52 +0200

    add taxidriversafetyapp

---
+++ galileoexperimentsextensions/taxidriversafetyapp/app.py
@@ -0,0 +1,73 @@
+import base64
+from typing import Dict
+
+import redis
+import requests
+from galileo.shell.shell import Galileo, ClientGroup
+from galileoexperiments.api.model import ProfilingWorkloadConfiguration
+from galileoexperiments.api.profiling import ProfilingApplication
+from kubernetes import client
+from kubernetes.client import V1ResourceRequirements, V1EnvVar, V1EnvVarSource, V1ObjectFieldSelector
+
+
+def _prepare_image(url: str):
+    r = requests.get(url)
+    r.raise_for_status()
+    return base64.b64encode(r.content).decode('utf-8')
+
+
+def _get_image(location: str, remote: bool) -> str:
+    if remote:
+        data = _prepare_image(location)
+    else:
+        data = _load_image_locally(location)
+    return data
+
+
+def _load_image_locally(path: str) -> str:
+    with open(path, 'rb') as fd:
+        return base64.b64encode(fd.read()).decode('utf-8')
+
+
+def _spawn_zone_group(zone: str, clients: int, g: Galileo, location: str, remote: bool, fn_name: str) -> ClientGroup:
+    return _spawn_group(g, clients, fn_name, f'{fn_name}-{zone}', location=location, labels={'galileo_zone': zone},
+                        remote=remote)
+
+
+def _spawn_group(g: Galileo, clients: int, fn_name: str, service_name: str, location: str, remote: bool,
+                 labels: dict = None):
+    path = f'/function/{fn_name}'
+    return g.spawn(service_name, clients,
+                   parameters={'method': 'post', 'path': path, 'kwargs': {'data': "%s" % _get_image(location, remote)}},
+                   worker_labels=labels)
+
+
+class TaxiDriverSafetyAppProfilingApplication(ProfilingApplication):
+
+    def spawn_group(self, clients: int, rds: redis.Redis, galileo: Galileo,
+                    config: ProfilingWorkloadConfiguration) -> ClientGroup:
+        zone = config.zone
+        location = config.params['service']['location']
+        fn_name = config.params['service']['name']
+        remote = bool(config.params['service']['remote'])
+        return _spawn_group(galileo, clients, fn_name, f'{fn_name}-{zone}', location=location, remote=remote,
+                            labels={'galileo_zone': zone})
+
+    def pod_factory(self, pod_name: str, image: str, resource_requests: Dict) -> client.V1Container:
+        return client.V1Container(
+            image=image,
+            name=pod_name,
+            ports=[
+                client.V1ContainerPort(
+                    name="function-port", container_port=8080
+                )
+            ],
+            resources=V1ResourceRequirements(
+                requests=resource_requests
+            ),
+            env=[
+                V1EnvVar('NODE_NAME', value_from=V1EnvVarSource(
+                    field_ref=(V1ObjectFieldSelector(field_path='spec.nodeName')))),
+                V1EnvVar('IMAGE_STORAGE', 'request')
+            ]
+        )
---
+++ galileoexperimentsextensions/taxidriversafetyapp/scenario/main.py
@@ -0,0 +1,92 @@
+import logging
+
+from galileo.shell.shell import init
+from galileo.worker.context import Context
+from galileoexperiments.api.model import ScenarioWorkloadConfiguration
+from galileoexperiments.experiment.scenario.run import run_scenario_workload
+
+from galileoexperimentsextensions.taxidriversafetyapp.app import TaxiDriverSafetyAppProfilingApplication
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    logging.basicConfig(level=logging._nameToLevel['INFO'])
+    creator = 'test'
+    taxidriversafetyapp_image = 'edgerun/taxidriversafetyapp:1.0.0'
+    humandetection_image = 'edgerun/humandetection:1.0.0'
+    gundetection_image = 'edgerun/gundetection:1.0.0'
+    maskdetection_image = 'edgerun/maskdetection:1.0.0'
+    app_names = {
+        taxidriversafetyapp_image: 'taxidriversafetyapp'
+    }
+
+    master_node = 'master-node'
+
+    # node to service mapping including the number of service instances
+    services = {
+        'test-node': {
+            humandetection_image: 1,
+            gundetection_image: 1,
+            maskdetection_image: 1,
+            taxidriversafetyapp_image: 1
+        }
+    }
+
+    # maps nodes that should host applications to zones
+    zone_mapping = {
+        'test-node': 'test-zone'
+    }
+
+    params = {}
+
+    # parameters for each image (used to initialize the clients)
+    app_params = {
+        taxidriversafetyapp_image: {
+            'service': {
+                'name': 'taxidriversafetyapp',
+                'location': 'https://i.imgur.com/bqnrbv6.jpg',
+                # 'location': 'https://i.imgur.com/XWPElmP.jpg',
+                'remote': True
+            }
+        }
+    }
+
+    profiling_apps = {
+        taxidriversafetyapp_image: TaxiDriverSafetyAppProfilingApplication()
+    }
+
+    # Instantiate galileo context that includes all dependencies needed to execute an experiment
+    ctx = Context()
+    rds = ctx.create_redis()
+    g = init(rds)
+
+    # contains 4 requests
+    test_profile = 'data/profiles/test.pkl'
+
+    # client profiles, each starts one client that sends to the zone's load balancer
+    profiles = {
+        'test-zone': {
+            taxidriversafetyapp_image: [test_profile, test_profile]
+        }
+    }
+
+    config = ScenarioWorkloadConfiguration(
+        creator=creator,
+        app_names=app_names,
+        master_node=master_node,
+        services=services,
+        zone_mapping=zone_mapping,
+        params=params,
+        app_params=app_params,
+        profiling_apps=profiling_apps,
+        context=g,
+
+        profiles=profiles
+    )
+
+    run_scenario_workload(config)
+
+
+if __name__ == '__main__':
+    main()

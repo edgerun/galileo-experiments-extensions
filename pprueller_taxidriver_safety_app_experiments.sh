e02b7ff6e963f178cecb742d81a84202ec1ecca5
Paul Prüller <>
2023-01-28 11:28:53 +0100

    add pic with mask

--- pprueller_taxidriver_safety_app_experiments.sh
+++ pprueller_taxidriver_safety_app_experiments.sh
@@ -1,14 +1,14 @@
 # taxidriversafetyapp new_york_1x1_1x1
-python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 1 eb-k3s-master taxidriversafetyapp-1-zone-min data/profiles/new_york_1x1_1x1_min https://i.imgur.com/CubSR2U.jpg
+python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 1 eb-k3s-master taxidriversafetyapp-1-zone-min data/profiles/new_york_1x1_1x1_min https://i.imgur.com/LpfkZ4D.jpg
 sleep 60
-python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 1 eb-k3s-master taxidriversafetyapp-1-zone-avg data/profiles/new_york_1x1_1x1_avg https://i.imgur.com/CubSR2U.jpg
+python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 1 eb-k3s-master taxidriversafetyapp-1-zone-avg data/profiles/new_york_1x1_1x1_avg https://i.imgur.com/LpfkZ4D.jpg
 sleep 60
-python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 1 eb-k3s-master taxidriversafetyapp-1-zone-max data/profiles/new_york_1x1_1x1_max https://i.imgur.com/CubSR2U.jpg
+python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 1 eb-k3s-master taxidriversafetyapp-1-zone-max data/profiles/new_york_1x1_1x1_max https://i.imgur.com/LpfkZ4D.jpg
 sleep 60
 
 # taxidriversafetyapp new_york_2x2_2x1
-python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 2 eb-k3s-master taxidriversafetyapp-2-zones-min data/profiles/new_york_2x2_2x1_min https://i.imgur.com/CubSR2U.jpg
+python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 2 eb-k3s-master taxidriversafetyapp-2-zones-min data/profiles/new_york_2x2_2x1_min https://i.imgur.com/LpfkZ4D.jpg
 sleep 60
-python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 2 eb-k3s-master taxidriversafetyapp-2-zones-avg data/profiles/new_york_2x2_2x1_avg https://i.imgur.com/CubSR2U.jpg
+python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 2 eb-k3s-master taxidriversafetyapp-2-zones-avg data/profiles/new_york_2x2_2x1_avg https://i.imgur.com/LpfkZ4D.jpg
 sleep 60
-python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 2 eb-k3s-master taxidriversafetyapp-2-zones-max data/profiles/new_york_2x2_2x1_max https://i.imgur.com/CubSR2U.jpg
+python -m galileoexperimentsextensions.taxidriversafetyapp.scenario.scenario pruellerpaul 2 eb-k3s-master taxidriversafetyapp-2-zones-max data/profiles/new_york_2x2_2x1_max https://i.imgur.com/LpfkZ4D.jpg

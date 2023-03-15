b9761d753d8a1b672298892986d5b85dad3e76a0
Paul Prüller <>
2023-03-15 21:44:54 +0100

    remove pose estimation

--- pprueller_experiments_startAll.sh
+++ pprueller_experiments_startAll.sh
@@ -46,22 +46,6 @@ sleep 60
 python -m galileoexperimentsextensions.maskdetection.scenario.scenario pruellerpaul edgerun/maskdetection:1.1.0 2 eb-k3s-master maskdetection data/profiles/new_york_2x2_2x1_max https://i.imgur.com/LpfkZ4D.jpg
 sleep 60
 
-# pose estimation new_york_2x2_1x1 (DONE)
-python -m galileoexperimentsextensions.poseestimation.scenario.scenario pruellerpaul edgerun/poseestimation:1.1.0 3 eb-k3s-master poseestimation data/profiles/new_york_2x2_1x1_min https://i.imgur.com/ztir9ww.jpg
-sleep 60
-python -m galileoexperimentsextensions.poseestimation.scenario.scenario pruellerpaul edgerun/poseestimation:1.1.0 3 eb-k3s-master poseestimation data/profiles/new_york_2x2_1x1_avg https://i.imgur.com/ztir9ww.jpg
-sleep 60
-python -m galileoexperimentsextensions.poseestimation.scenario.scenario pruellerpaul edgerun/poseestimation:1.1.0 3 eb-k3s-master poseestimation data/profiles/new_york_2x2_1x1_max https://i.imgur.com/ztir9ww.jpg
-sleep 60
-
-# pose estimation new_york_2x2_2x1 (DONE)
-python -m galileoexperimentsextensions.poseestimation.scenario.scenario pruellerpaul edgerun/poseestimation:1.1.0 2 eb-k3s-master poseestimation data/profiles/new_york_2x2_2x1_min https://i.imgur.com/ztir9ww.jpg
-sleep 60
-python -m galileoexperimentsextensions.poseestimation.scenario.scenario pruellerpaul edgerun/poseestimation:1.1.0 2 eb-k3s-master poseestimation data/profiles/new_york_2x2_2x1_avg https://i.imgur.com/ztir9ww.jpg
-sleep 60
-python -m galileoexperimentsextensions.poseestimation.scenario.scenario pruellerpaul edgerun/poseestimation:1.1.0 2 eb-k3s-master poseestimation data/profiles/new_york_2x2_2x1_max https://i.imgur.com/ztir9ww.jpg
-sleep 60
-
 # object detection new_york_1x1_1x1 (DONE)
 python -m galileoexperimentsextensions.objectdetection.scenario.scenario pruellerpaul edgerun/objectdetection:1.1.0 1 eb-k3s-master objectdetection data/profiles/new_york_1x1_1x1_min https://i.imgur.com/LpfkZ4D.jpg
 sleep 60

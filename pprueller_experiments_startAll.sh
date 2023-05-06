00eb14051c1b96300899d41014560c57a5c09b1a
Paul Prüller <>
2023-05-06 16:07:16 +0200

    change sleep time

--- pprueller_experiments_startAll.sh
+++ pprueller_experiments_startAll.sh
@@ -1,62 +1,62 @@
 # humandetection new_york_1x1_1x1 (DONE)
 python -m galileoexperimentsextensions.humandetection.scenario.scenario pruellerpaul edgerun/humandetection:1.1.0 1 eb-k3s-master humandetection data/profiles/new_york_1x1_1x1_min https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.humandetection.scenario.scenario pruellerpaul edgerun/humandetection:1.1.0 1 eb-k3s-master humandetection data/profiles/new_york_1x1_1x1_avg https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.humandetection.scenario.scenario pruellerpaul edgerun/humandetection:1.1.0 1 eb-k3s-master humandetection data/profiles/new_york_1x1_1x1_max https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 
 # humandetection new_york_1x1_05x05 (DONE)
 python -m galileoexperimentsextensions.humandetection.scenario.scenario pruellerpaul edgerun/humandetection:1.1.0 3 eb-k3s-master humandetection data/profiles/new_york_1x1_05x05_min https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.humandetection.scenario.scenario pruellerpaul edgerun/humandetection:1.1.0 3 eb-k3s-master humandetection data/profiles/new_york_1x1_05x05_avg https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.humandetection.scenario.scenario pruellerpaul edgerun/humandetection:1.1.0 3 eb-k3s-master humandetection data/profiles/new_york_1x1_05x05_max https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 
 # gundetection new_york_1x1_1x1 (DONE)
 python -m galileoexperimentsextensions.gundetection.scenario.scenario pruellerpaul edgerun/gundetection:1.1.0 1 eb-k3s-master gundetection data/profiles/new_york_1x1_1x1_min https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.gundetection.scenario.scenario pruellerpaul edgerun/gundetection:1.1.0 1 eb-k3s-master gundetection data/profiles/new_york_1x1_1x1_avg https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.gundetection.scenario.scenario pruellerpaul edgerun/gundetection:1.1.0 1 eb-k3s-master gundetection data/profiles/new_york_1x1_1x1_max https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 
 # gundetection new_york_1x1_05x05 (DONE)
 python -m galileoexperimentsextensions.gundetection.scenario.scenario pruellerpaul edgerun/gundetection:1.1.0 3 eb-k3s-master gundetection data/profiles/new_york_1x1_05x05_min https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.gundetection.scenario.scenario pruellerpaul edgerun/gundetection:1.1.0 3 eb-k3s-master gundetection data/profiles/new_york_1x1_05x05_avg https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.gundetection.scenario.scenario pruellerpaul edgerun/gundetection:1.1.0 3 eb-k3s-master gundetection data/profiles/new_york_1x1_05x05_max https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 
 # mask detection new_york_2x2_1x1 (DONE)
 python -m galileoexperimentsextensions.maskdetection.scenario.scenario pruellerpaul edgerun/maskdetection:1.1.0 3 eb-k3s-master maskdetection data/profiles/new_york_2x2_1x1_min https://i.imgur.com/LpfkZ4D.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.maskdetection.scenario.scenario pruellerpaul edgerun/maskdetection:1.1.0 3 eb-k3s-master maskdetection data/profiles/new_york_2x2_1x1_avg https://i.imgur.com/LpfkZ4D.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.maskdetection.scenario.scenario pruellerpaul edgerun/maskdetection:1.1.0 3 eb-k3s-master maskdetection data/profiles/new_york_2x2_1x1_max https://i.imgur.com/LpfkZ4D.jpg
-sleep 60
+sleep 30
 
 # mask detection new_york_2x2_2x1 (DONE)
 python -m galileoexperimentsextensions.maskdetection.scenario.scenario pruellerpaul edgerun/maskdetection:1.1.0 2 eb-k3s-master maskdetection data/profiles/new_york_2x2_2x1_min https://i.imgur.com/LpfkZ4D.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.maskdetection.scenario.scenario pruellerpaul edgerun/maskdetection:1.1.0 2 eb-k3s-master maskdetection data/profiles/new_york_2x2_2x1_avg https://i.imgur.com/LpfkZ4D.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.maskdetection.scenario.scenario pruellerpaul edgerun/maskdetection:1.1.0 2 eb-k3s-master maskdetection data/profiles/new_york_2x2_2x1_max https://i.imgur.com/LpfkZ4D.jpg
-sleep 60
+sleep 30
 
 # object detection new_york_1x1_1x1 (DONE)
 python -m galileoexperimentsextensions.objectdetection.scenario.scenario pruellerpaul edgerun/objectdetection:1.1.0 1 eb-k3s-master objectdetection data/profiles/new_york_1x1_1x1_min https://i.imgur.com/LpfkZ4D.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.objectdetection.scenario.scenario pruellerpaul edgerun/objectdetection:1.1.0 1 eb-k3s-master objectdetection data/profiles/new_york_1x1_1x1_avg https://i.imgur.com/LpfkZ4D.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.objectdetection.scenario.scenario pruellerpaul edgerun/objectdetection:1.1.0 1 eb-k3s-master objectdetection data/profiles/new_york_1x1_1x1_max https://i.imgur.com/LpfkZ4D.jpg
-sleep 60
+sleep 30
 
 # object detection new_york_1x1_05x05
 python -m galileoexperimentsextensions.objectdetection.scenario.scenario pruellerpaul edgerun/objectdetection:1.1.0 3 eb-k3s-master objectdetection data/profiles/new_york_1x1_05x05_min https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.objectdetection.scenario.scenario pruellerpaul edgerun/objectdetection:1.1.0 3 eb-k3s-master objectdetection data/profiles/new_york_1x1_05x05_avg https://i.imgur.com/ztir9ww.jpg
-sleep 60
+sleep 30
 python -m galileoexperimentsextensions.objectdetection.scenario.scenario pruellerpaul edgerun/objectdetection:1.1.0 3 eb-k3s-master objectdetection data/profiles/new_york_1x1_05x05_max https://i.imgur.com/ztir9ww.jpg
